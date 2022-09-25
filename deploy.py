f98a6d7998b89f642c6b566cdbed297ba78a5f8f
amatureApe <amatureApe@gmail.com>
2022-09-25 15:07:27 -0500

    build transaction and adjust for pre-EIP1599 txs

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 from solcx import compile_standard, install_solc
 import json
+from web3 import Web3
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -33,4 +34,27 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 # get abi
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
-print(abi)
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://0.0.0.0:8545"))
+chain_id = 1337
+my_address = "0xd45D4f78aeBAA5c9254dba282ac42c51DE21C09A"
+private_key = "0x755fc663b45ad7d73e78983e2e830f817de7d87c54938c9278021e6f6a5693e3"
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+print(transaction)
